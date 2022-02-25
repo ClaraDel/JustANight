e99fa9ef326142f8eeedaf41347bb416ac7e8dc8
IlonaPw <74210552+IlonaPw@users.noreply.github.com>
2022-02-25 18:12:57 -0500

    Triiger sous-Titres

--- Assets/AssetJustANight/Script/Chambre.cs
+++ Assets/AssetJustANight/Script/Chambre.cs
@@ -7,25 +7,41 @@ public class Chambre : MonoBehaviour
 {
     [SerializeField] private GameObject objectifPrefab;
     [SerializeField] private GameObject panel;
+    [SerializeField] private GameObject sousTitrePrefab;
+    [SerializeField] private GameObject panelTitre;
 
     private GameObject Message;
+    private GameObject sous_titre;
 
     private bool isActivated = false;
     private bool hasBeenActivated = false;
+    bool activateOnStart = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && !isActivated && !hasBeenActivated)
         {
+            Activate_Message();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !isActivated && !hasBeenActivated)
+        {
             Activate();
         }
     }
-
+    public void Activate_Message()
+    {
+        sous_titre = Instantiate(sousTitrePrefab, panelTitre.transform.position, new Quaternion(0, 0, 0, 0), panelTitre.transform);
+        sous_titre.GetComponentInChildren<TMP_Text>().text = "Mom, Dad ?";
+        Destroy(sous_titre, 5f);
+    }
     public void Activate()
     {
         Message = Instantiate(objectifPrefab, panel.transform.position, new Quaternion(0, 0, 0, 0), panel.transform);
