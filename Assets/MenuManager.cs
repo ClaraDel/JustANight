2b3fc6320b036e320af21b17d4dac57ffc38965e
Nomanina <70279300+Nomanina@users.noreply.github.com>
2022-02-26 18:58:46 -0500

    audio option

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
 	[SerializeField]GameObject[] showOnPauseObjects;
 	[SerializeField] GameObject[] hideOnPauseObjects;
+
+	[SerializeField] GameObject exitPopup;
+
+	[SerializeField] GameObject mainMenu;
+	[SerializeField] GameObject settings;
+
+	[SerializeField] TMP_Text textVolume;
+	[SerializeField] Slider volumeSlider;
+	[SerializeField] GameObject audioPopup;
+
+	float defaultVolume;
+	float currentVolume;
+
 	bool canPause = false;
 
 	IEnumerator waitCinematic()
@@ -20,6 +35,10 @@ public class MenuManager : MonoBehaviour
 		Time.timeScale = 1;
 		hidePaused();
 		StartCoroutine(waitCinematic());
+		volumeSlider.value = AudioListener.volume*50;
+		textVolume.text = (AudioListener.volume * 50).ToString("0.0");
+		defaultVolume =AudioListener.volume;
+		currentVolume = defaultVolume;
 	}
 
 	// Update is called once per frame
@@ -106,4 +125,71 @@ public class MenuManager : MonoBehaviour
 		Application.Quit();
 		UnityEditor.EditorApplication.isPlaying = false;
 	}
+
+	public void openExitPopup()
+    {
+		exitPopup.SetActive(true);
+    }
+
+	public void closeExitPopup()
+    {
+		exitPopup.SetActive(false);
+    }
+
+	public void showAudioSetting()
+    {
+		volumeSlider.value = currentVolume * 50;
+		textVolume.text = (currentVolume * 50).ToString("0.0");
+		audioPopup.SetActive(true);
+		
+    }
+
+	public void hideAudioSetting()
+    {
+		currentVolume = AudioListener.volume;
+		audioPopup.SetActive(false);
+    }
+
+	public void setVolume()
+    {
+		float value = volumeSlider.value;
+		AudioListener.volume = value/50;
+		textVolume.text = (value).ToString("0.0");
+    }
+
+	public void exitVolumeWithoutUpdate()
+    {
+		AudioListener.volume = currentVolume;
+		audioPopup.SetActive(false);
+    }
+
+	public void resetVolume()
+    {
+		AudioListener.volume = defaultVolume;
+		textVolume.text = (defaultVolume * 50).ToString("0.0");
+		volumeSlider.value = defaultVolume * 50;
+	
+    }
+
+	public void setDefaultVolume()
+    {
+		AudioListener.volume = defaultVolume;
+		volumeSlider.value = defaultVolume * 50;
+		textVolume.text = (defaultVolume*50).ToString("0.0");
+
+	}
+
+	public void settings2mainMenu()
+    {
+		mainMenu.SetActive(true);
+		settings.SetActive(false);
+		
+    }
+
+	public void mainMenu2settings()
+	{
+		mainMenu.SetActive(false);
+		settings.SetActive(true);
+	}
+
 }
